1bef0831a63fe319ce5ff09976c8d7666a595093
Brandon Wright <thewrightbrandon@gmail.com>
2021-05-26 10:20:47 -0400

    merge

--- django_ATF/django_ATF/settings.py
+++ django_ATF/django_ATF/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'moods',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'allday1',
         'HOST': 'localhost'
     }
 }
