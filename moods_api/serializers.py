c4cba899fbcac6fec59fe5d9443378128d536aab
Brandon Wright <thewrightbrandon@gmail.com>
2021-05-28 16:36:45 -0400

    Trying related models

--- moods_api/serializers.py
+++ moods_api/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from .models import Mood
-from .models import Comment
 
 class MoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mood
         fields = ('id', 'title', 'emotion', 'notes', 'created_at',)
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ('id', 'name', 'comment', 'mood_id')
