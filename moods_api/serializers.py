cb25e1850782f25492457ccbbd44b372c381aa61
Brandon Wright <thewrightbrandon@gmail.com>
2021-05-28 18:57:13 -0400

    Added Comment Model

--- moods_api/serializers.py
+++ moods_api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Mood
+from .models import Comments
 
 class MoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mood
         fields = ('id', 'title', 'emotion', 'notes', 'created_at',)
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ('id', 'mood', 'name', 'body',)
